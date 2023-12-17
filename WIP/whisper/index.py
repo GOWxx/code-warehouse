114ddd91946a9cf57f7d5da4d357aac4cb9b51db
GOWxx <sunbowen523@gmail.com>
2023-12-17 23:46:32 +0800

    something forgot

--- WIP/whisper/index.py
+++ WIP/whisper/index.py
@@ -2,5 +2,10 @@ import whisper
 
 model = whisper.load_model("base")
 # 这里是 .mp3 或者 .mp4 都可以
-result = model.transcribe("./demo/audio_test.mp4", fp16=False, language='English')
-print(result["text"])
+result = model.transcribe("./demo/tiktok.m4a", fp16=False, language='English')
+
+# print(result["text"])
+
+# 将结果保存到文本文件
+with open('transcription.txt', 'w') as f:
+    f.write(result["text"])
