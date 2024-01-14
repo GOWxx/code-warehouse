5cbc1d583c5fadcaf2ea92a433ba564fb4cb6e7d
GOWxx <sunbowen523@gmail.com>
2024-01-14 19:13:44 +0800

    update

--- WIP/whisper/index.py
+++ WIP/whisper/index.py
@@ -1,11 +1,11 @@
 import whisper
 
-model = whisper.load_model("base")
+model = whisper.load_model("large-v3")
 # 这里是 .mp3 或者 .mp4 都可以
-result = model.transcribe("./demo/Test1.Section1.mp3", fp16=False)
+result = model.transcribe("./demo/0401680564.mp3", fp16=False)
 
 print(result["text"])
 
 # 将结果保存到文本文件
-with open('Test1.Section1.txt', 'w') as f:
+with open('0401680564.txt', 'w') as f:
     f.write(result["text"])
